c0a22d9ef513b5c64e1e27130da027b9f889b60e
François Bullier <fbullier@360client.fr>
2024-12-23 19:53:01 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
+required = Path("requirements.txt").read_text(encoding="utf-8").split("\n")
+
 setup(
+
     name="beevibe",
     version="0.1.0",
-    description="A Python package for training and inference of language models on thematic datasets.",
-    author="BeeVibe Authors",
+    description="A lightweight framework for training and deploying language models for thematic text classification.",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    author="François Bullier",
+    author_email="fbullier@360client.fr",
+    url="https://github.com/FBullier/beevibe",
+    packages=find_packages(exclude=["tests", "tests.*"]),  # same as name
     license="MIT",
-    packages=find_packages(),
-    python_requires=">=3.10.12",
-    install_requires=[
-        "pandas>=2.2.2",
-        "numpy>=1.26.4",
-        "scipy>=1.13.1",
-        "scikit-learn>=1.6.0",
-        "torch>=2.5.1",
-        "transformers>=4.47.0",
-        "tokenizers>=0.21.0",
-        "datasets>=3.2.0",
-        "seaborn>=0.11.0",
-        "matplotlib>=3.3.0",
-        "pydantic>=1.10.0",
-    ],
+
+    install_requires=required,
     extras_require={
         "dev": [
             "pytest",
@@ -28,12 +25,12 @@ setup(
             "black",
         ]
     },
+    include_package_data=True,    
+    python_requires=">=3.10.12",
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    include_package_data=True,
 )
